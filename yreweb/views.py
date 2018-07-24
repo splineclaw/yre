4bf62b2d9bf4236905dfb5f5cfddb6c158183454
splineclaw <37070764+splineclaw@users.noreply.github.com>
2018-07-23 22:29:40 -0400

    wip local image hosting

--- yreweb/views.py
+++ yreweb/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .yre.analysis import get_ten_similar
 from .yre.database import Database
 from .yre import constants
+from .yre import images
 
 import time
 
@@ -23,28 +24,44 @@ def urls_list(request, source_id):
 
 def similar_pics(request, source_id,
                  stale_time=constants.DEFAULT_STALE_TIME,
-                 full=False):
+                 full=False, source='local'):
     start = time.time()
     similar_ids = get_ten_similar(source_id, stale_time, from_full=full)[:8]
     db = Database()
-    urls = db.get_urls_for_ids(similar_ids)
 
-    urls_preview = []
-    for u in urls:
-        slicepoint = u.find('/data/')+len('/data/')
-        new_url = u[:slicepoint]
-        new_url += 'sample/'
-        new_url += u[slicepoint:]
-        urls_preview.append(new_url)
+    if source == 'remote':
+        urls = db.get_urls_for_ids(similar_ids)
 
-    e6prefix = 'http://localhost:8000/'#'https://e621.net/post/show/'
-    e6urls = [e6prefix+str(id) for id in similar_ids]
+        urls_preview = []
+        for u in urls:
+            slicepoint = u.find('/data/')+len('/data/')
+            new_url = u[:slicepoint]
+            new_url += 'sample/'
+            new_url += u[slicepoint:]
+            urls_preview.append(new_url)
 
-    zipped = list(zip(similar_ids, urls, e6urls))
+        e6prefix = 'http://localhost:8000/'#'https://e621.net/post/show/'
+        e6urls = [e6prefix+str(id) for id in similar_ids]
 
-    context = {'source': source_id, 'zipped': zipped}
-    print('response for {} took {}s'.format(source_id, time.time()-start))
-    return render(request, 'yreweb/response.html', context)
+        zipped = list(zip(similar_ids, urls, e6urls))
+
+        context = {'source': source_id, 'zipped': zipped}
+        print('response for {} took {}s'.format(source_id, time.time()-start))
+        return render(request, 'yreweb/response-remote.html', context)
+
+    elif source == 'local':
+        names = [images.image_with_delay(id) for id in similar_ids]
+
+        link_prefix = 'http://localhost:8000/'
+        link_urls = [link_prefix+str(id) for id in similar_ids]
+
+        zipped = list(zip(similar_ids, names, link_urls))
+
+        print('response for {} took {}s'.format(source_id, time.time()-start))
+        context = {'source': source_id, 'zipped': zipped}
+        return render(request, 'yreweb/response-local.html', context)
+
+
 
 def recompute_similar(request, source_id):
     return similar_pics(request, source_id, stale_time=0)
---
+++ yreweb/yre/images.py
@@ -0,0 +1,75 @@
+import requests
+from requests.packages.urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
+
+from os.path import isfile, dirname, abspath
+import inspect
+from pathlib import Path
+from shutil import copyfile
+
+import time
+
+try:
+    from database import Database
+    import constants
+except ModuleNotFoundError:
+    from .database import Database
+    from . import constants
+
+import urllib
+
+def get_local(post_id, return_type='filename'):
+    '''
+    For a post id (12345), return its filename ('12345.jpg') as stored locally,
+    downloading it from e621 if necessary.
+
+    TODO: animation support
+    '''
+    self_path = dirname(abspath(inspect.getfile(inspect.currentframe())))
+    previews_path = str(Path(self_path).parent) + '/static/yreweb/previews/'
+
+    filename = str(post_id) + '.jpg'
+    local_image = previews_path + filename
+
+    if isfile(local_image):
+        if return_type == 'filename':
+            return filename
+        elif return_type == 'cachehit':
+            return (filename, 1)
+
+    db = Database()
+    urls = db.get_urls_for_ids([post_id])
+    file_url = urls[0]
+
+    prefix = 'https://static1.e621.net/data/'
+    probable_sample_url = prefix + 'sample/' + file_url[len(prefix):-3] + 'jpg'
+
+    try:
+        urllib.request.urlretrieve(probable_sample_url, local_image)
+        print('Downloaded', post_id)
+    except urllib.error.HTTPError:
+        print('Could not retrieve preview for', post_id)
+        copyfile(previews_path+'error.jpg',local_image)
+        if return_type == 'filename':
+            return filename
+        elif return_type == 'cachehit':
+            return (filename, 0)
+
+def image_with_delay(post_id):
+    start = time.time()
+    r = []
+    i = 0
+    while not r:
+        r = get_local(post_id, 'cachehit')
+        if not r:
+            t = time.time()
+            while time.time() - t < constants.REQUEST_DELAY * 2**i:
+                time.sleep(0.01)
+            i += 1
+
+
+    name, hit = r
+    if not hit:
+        while time.time() - start < constants.REQUEST_DELAY:
+            time.sleep(0.01)
+    return name
