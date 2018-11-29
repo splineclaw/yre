a6cb59bd374492405a05d9adc6db1102f5f5a0b5
splineclaw <37070764+splineclaw@users.noreply.github.com>
2018-11-29 06:53:41 -0500

    broaden scope

--- yreweb/yre/constants.py
+++ yreweb/yre/constants.py
@@ -8,10 +8,10 @@ PAGE_DELAY = 2  # 30 per minute
 REQUEST_DELAY = 0.5  # 120 per minute
 FAV_REQ_TIMEOUT = 2  # seconds
 
-MIN_FAVS = 200
+MIN_FAVS = 25
 SUBSET_FAVS_PER_POST = 256
 BRANCH_FAVS_MIN = 5
 BRANCH_FAVS_COEFF = 0.2 # only top n posts by branch favs will be analysed
-BRANCH_FAVS_MAX = 2000 # ... or this number, whichever is lesser
+BRANCH_FAVS_MAX = 5000 # ... or this number, whichever is lesser
 
 DEFAULT_STALE_TIME = 10**7 # seconds
