894adc2a97d584409e661770e9f466ae811f0f1f
splineclaw <37070764+splineclaw@users.noreply.github.com>
2018-12-02 03:26:21 -0500

    change example post and similar count

--- yreweb/yre/constants.py
+++ yreweb/yre/constants.py
@@ -1,7 +1,7 @@
 VERSION = '0.0.07'
 
 USER_AGENT = 'yre {} (splineclaw)'.format(VERSION)
-EXAMPLE_POST_ID = 912182
+EXAMPLE_POST_ID = 849988
 
 # rate limiting
 PAGE_DELAY = 2  # 30 per minute
@@ -14,8 +14,8 @@ BRANCH_FAVS_MIN = 5
 BRANCH_FAVS_COEFF = 1 # only this fraction of top posts by branch favs will be analysed
 BRANCH_FAVS_MAX = 1000 # ... or this number, whichever is lesser
 SYM_SIM_MODE = 'add_sim' # 'add_sim' or 'mult_sim'. see analysis.sym_sims for details.
-SIM_PER_POST = 100 # store n similars per post
-SIMS_SHOWN = 10 # show n similars per post
+SIM_PER_POST = 25 # store n similars per post
+SIMS_SHOWN = 25 # show n similars per post
 PRE_DOWNLOAD = True # download SIM_PER_POST posts during presampling
 
 DEFAULT_STALE_TIME = 10**7 # seconds
